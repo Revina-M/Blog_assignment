6707aafbe75387993b8789772aa2006ed39cf289
Revina-M <revina95rm@gmail.com>
2022-02-10 11:11:56 +0530

    cc

--- index.js
+++ index.js
@@ -5,7 +5,7 @@
     require('dotenv').config();
 
     const app = express();
-    const port = process.env.PORT || 8080;
+    // const port = process.env.PORT || 8080;
 
     app.use(cors());
     app.use(express.json());
@@ -35,4 +35,6 @@ app.get("*", function (request, response) {
   response.sendFile(path.resolve(__dirname, "./blog_frontend/build", "index.html"));
 });
 
-    app.listen(port,()=>console.log(`The app is running on Port: ${port}`));
+http.listen((process.env.PORT || 8080), function(){
+    console.log('listening on *:8080');
+  });
